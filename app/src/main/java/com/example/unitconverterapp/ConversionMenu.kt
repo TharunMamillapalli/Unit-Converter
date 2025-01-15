3d8590cc84c311376249f731b77ef8ff3844881b
tharun <tharunmamillapalli@gmail.com>
2025-01-15 11:16:32 -0600

    Creating the composables : Part 1

---
+++ app/src/main/java/com/example/unitconverterapp/ConversionMenu.kt
@@ -0,0 +1,71 @@
+package com.example.unitconverterapp
+
+
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+
+@Composable
+fun ConversionMenu(list: List<Conversion>,modifier: Modifier=Modifier) {
+    var displayingText by remember{ mutableStateOf("Select the Conversion type") }
+    var textFieldSize by remember { mutableStateOf(Size.Zero) }
+    var expanded by remember { mutableStateOf(false) }
+    val icon=if(expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    OutlinedTextField(
+        value = displayingText, onValueChange ={displayingText=it},
+        textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+        modifier = Modifier.fillMaxWidth()
+            .onGloballyPositioned {cordinates->
+                textFieldSize=cordinates.size.toSize()
+            },
+        label = { Text(text = "Conversion Type") },
+        trailingIcon = {
+            Icon(icon, contentDescription = "icon",
+            modifier.clickable { expanded=!expanded })
+        },
+        readOnly = true
+        )
+    DropdownMenu(
+        expanded=expanded, onDismissRequest = {expanded=false},
+        modifier=modifier.width(with(LocalDensity.current){textFieldSize.width.toDp()})
+    ) {
+        list.forEach {conversion ->
+            DropdownMenuItem(
+                onClick = {
+                    expanded=false
+                    displayingText=conversion.description
+                },
+                text={ Text(text = conversion.description) }
+            )
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/unitconverterapp/TopScreen.kt
@@ -0,0 +1,9 @@
+package com.example.unitconverterapp
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TopScreen(list: List<Conversion>){
+    ConversionMenu(list=list)
+
+}
